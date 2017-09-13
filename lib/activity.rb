2e18478bb75c123cf34fc62d53d94eb1bb148e97
Luis Senior <heybluguy@yahoo.com>
2017-09-13 09:54:21 -0600

    reunion

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,11 @@
 class Activity
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def name
+    @name
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,12 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new("dinner")
+
+    assert_equal "dinner", activity.name
+  end
+
+  
+
 end
